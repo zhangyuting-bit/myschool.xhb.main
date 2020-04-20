e2d7789b3933caae29d9d151f023bad46754f47d
meng <935330004@qq.com>
2020-04-20 09:26:17 +0800

    mc_dev04 14 提交的功能 接口

--- myschool.xhb.main/myshool.class.api/src/main/java/com/zb/feign/ClassMassagesFeign.java
+++ myschool.xhb.main/myshool.class.api/src/main/java/com/zb/feign/ClassMassagesFeign.java
@@ -1,7 +1,6 @@
 package com.zb.feign;
 
 import com.zb.dto.Dto;
-import com.zb.dto.DtoUtil;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
