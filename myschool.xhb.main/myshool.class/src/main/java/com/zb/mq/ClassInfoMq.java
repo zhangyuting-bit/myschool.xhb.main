197fcd5466a239c8526f29cf26db7efedc607db4
meng <935330004@qq.com>
2020-04-21 18:26:59 +0800

    mc_dev04 14 提交的功能 接口

--- myschool.xhb.main/myshool.class/src/main/java/com/zb/mq/ClassInfoMq.java
+++ myschool.xhb.main/myshool.class/src/main/java/com/zb/mq/ClassInfoMq.java
@@ -2,9 +2,10 @@ package com.zb.mq;
 
 import com.rabbitmq.client.Channel;
 import com.zb.config.RabbitConfig;
-import com.zb.pojo.Class_Job;
+import com.zb.feign.UserFeignClient;
 import com.zb.pojo.Class_add;
 import com.zb.pojo.Class_info;
+import com.zb.pojo.UserInfo;
 import com.zb.service.ClassInfoService;
 import com.zb.service.ClassleaveService;
 import com.zb.util.IdWorker;
@@ -20,15 +21,19 @@ import java.io.IOException;
 public class ClassInfoMq {
     @Autowired
     private ClassInfoService classInfoService;
+    @Autowired
+    private UserFeignClient userFeignClient;
     @RabbitListener(queues = RabbitConfig.CLASSQUEUE)
-    public void send(Class_add classAdd, Message message, org.springframework.messaging.Message messages,Channel channel){
+    public void send(Class_add classAdd,Message message, org.springframework.messaging.Message messages,Channel channel){
         Long tag= (Long)messages.getHeaders().get(AmqpHeaders.DELIVERY_TAG);
+        UserInfo userInfoById = userFeignClient.getUserInfoById(classAdd.getTeacher_id());
         Class_info classInfo=new Class_info();
         classInfo.setId(IdWorker.getId());
-        classInfo.setCall("孟老师"); //这个地方调用登录的feign来传用户的名称
-        classInfo.setUser_id(classAdd.getTeacher_id());
+        classInfo.setCall(userInfoById.getName()); //这个地方调用登录的feign来传用户的名称
+        classInfo.setUser_id(userInfoById.getCodeType());
         classInfo.setClass_number(classAdd.getClass_number());
         classInfo.setJurisdiction("最高管理员"); //一开始添加 -1表示在本班级的最高级别
+        classInfo.setRelationship("");
         classInfo.setClass_subject(classAdd.getClass_subject());
         classInfo.setState(1);
         try {
